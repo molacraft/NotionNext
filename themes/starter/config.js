e9e4494d3702a4a4542be7ef790a5983e41e4a38
molacraft <info@molacraft.com>
2025-03-11 21:56:20 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,8 +10,8 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  STARTER_HERO_TITLE_1: 'At Mola Craft, you can get following custom products at direct factory price with one-stop solution reliable service.', // 英雄区文字
+  STARTER_HERO_TITLE_2: 'Crafts of different kinds and designs.', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
