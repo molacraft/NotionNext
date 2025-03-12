ecdef17711ac5bb2b860c1bc6fd4c9d0ee2e15ec
molacraft <info@molacraft.com>
2025-03-12 13:57:08 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -61,19 +61,19 @@ const CONFIG = {
 
   // 首页ABOUT区块
   STARTER_ABOUT_ENABLE: true, // ABOUT区块开关
-  STARTER_ABOUT_TITLE: '一套轻量实用的建站解决方案',
+  STARTER_ABOUT_TITLE: 'ABOUT MOLA CRAFT',
   STARTER_ABOUT_TEXT:
-    'NotionNext的愿景是帮助非技术人员的小白，最低成本、最快速地搭建自己的网站，帮助您将自己的产品与故事高效地传达给世界。 <br /> <br /> 功能强大的Notion笔记，简单快速的Vercel托管平台，组成一个简单的网站',
-  STARTER_ABOUT_BUTTON_TEXT: '了解更多',
-  STARTER_ABOUT_BUTTON_URL: 'https://docs.tangly1024.com/about',
+    'We have been specializing in custom craft & gift for over 7 years. <br /> We have a production team of more than 200 employees, and a total of about 50 people in the art team and sales team. <br /> So our working language is English and all your quotations and inquiries will be replied on the same working day.<br />In addition, our artist team has more than 5 employees, they work with the sales team in the same office to help our sales to convert your idea or sketch into workable production proof.<br />Our team will keep hard &smart working to create more value to help you win more orders. ',  // <br /> 代表换行
+  STARTER_ABOUT_BUTTON_TEXT: 'Learn more',
+  STARTER_ABOUT_BUTTON_URL: '', // 此处可链接到关于我们的介绍
   STARTER_ABOUT_IMAGE_1: '/images/starter/about/about-image-01.jpg',
   STARTER_ABOUT_IMAGE_2: '/images/starter/about/about-image-02.jpg',
-  STARTER_ABOUT_TIPS_1: '7000+',
-  STARTER_ABOUT_TIPS_2: '博客站点',
-  STARTER_ABOUT_TIPS_3: '正在线上运行',
+  STARTER_ABOUT_TIPS_1: 'xxx',
+  STARTER_ABOUT_TIPS_2: 'xxx',
+  STARTER_ABOUT_TIPS_3: 'xxx',
 
   // 首页价格区块
-  STARTER_PRICING_ENABLE: true, // 价格区块开关
+  STARTER_PRICING_ENABLE: false, // 价格区块开关 ture / False
   STARTER_PRICING_TITLE: '价格表',
   STARTER_PRICING_TEXT_1: '很棒的定价计划',
   STARTER_PRICING_TEXT_2:
