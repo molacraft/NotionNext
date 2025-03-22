ef33fc43ba69ae1087261ab7969b54fd116fb253
molacraft <info@molacraft.com>
2025-03-22 13:00:44 +0800

    Update Footer.js

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -138,7 +138,7 @@ export const Footer = props => {
                       rel='' //nofollow noopner noreferrer
                       target='_blank'
                       className='px-1 text-gray-1 hover:underline'>
-                      molacraft //{siteConfig('VERSION')}
+                      MOLACRAFT // {siteConfig('VERSION')}
                     </a>
                   </p>
                 </div>
