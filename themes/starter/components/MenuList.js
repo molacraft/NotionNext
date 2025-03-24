cb90738df7954393cb5fd2170793701bbcd4204b
molacraft <info@molacraft.com>
2025-03-24 13:39:08 +0800

    Update MenuList.js

--- themes/starter/components/MenuList.js
+++ themes/starter/components/MenuList.js
@@ -1,19 +1,15 @@
-import BLOG from '@/blog.config'
-import { siteConfig } from '@/lib/config'
-import { useGlobal } from '@/lib/global'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { MenuItem } from './MenuItem'
+import { siteConfig } from '@/lib/config';
+import { useGlobal } from '@/lib/global';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { MenuItem } from './MenuItem';
 
-/**
- * 响应式 折叠菜单
- */
 export const MenuList = props => {
-  const { customNav, customMenu } = props
-  const { locale } = useGlobal()
+  const { customNav, customMenu } = props;
+  const { locale } = useGlobal();
 
-  const [showMenu, setShowMenu] = useState(false) // 控制菜单展开/收起状态
-  const router = useRouter()
+  const [showMenu, setShowMenu] = useState(false); // 控制菜单展开/收起状态
+  const router = useRouter();
 
   let links = [
     {
@@ -40,54 +36,52 @@ export const MenuList = props => {
       href: '/tag',
       show: siteConfig('HEO_MENU_TAG')
     }
-  ]
+  ];
 
   if (customNav) {
-    links = customNav.concat(links)
+    links = customNav.concat(links);
   }
 
-  // 如果 开启自定义菜单，则覆盖Page生成的菜单
-  if (siteConfig('CUSTOM_MENU', BLOG.CUSTOM_MENU)) {
-    links = customMenu
+  if (siteConfig('CUSTOM_MENU')) {
+    links = customMenu;
   }
 
   if (!links || links.length === 0) {
-    return null
+    return null;
   }
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu) // 切换菜单状态
-  }
+    setShowMenu(!showMenu);
+  };
 
   useEffect(() => {
-    setShowMenu(false)
-  }, [router])
+    setShowMenu(false);
+  }, [router]);
 
   return (
     <div>
-      {/* 移动端菜单切换按钮 */}
       <button
-        id='navbarToggler'
+        id="navbarToggler"
         onClick={toggleMenu}
         className={`absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden ${
           showMenu ? 'navbarTogglerActive' : ''
         }`}>
-        <span className='relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all'></span>
-        <span className='relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all'></span>
-        <span className='relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all'></span>
+        <span className="relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all"></span>
+        <span className="relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all"></span>
+        <span className="relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all"></span>
       </button>
 
       <nav
-        id='navbarCollapse'
+        id="navbarCollapse"
         className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:px-4 lg:py-0 lg:shadow-none dark:lg:bg-transparent xl:px-6 ${
           showMenu ? '' : 'hidden'
         }`}>
-        <ul className='blcok lg:flex 2xl:ml-20'>
-          {links?.map((link, index) => (
+        <ul className="block lg:flex 2xl:ml-20">
+          {links.map((link, index) => (
             <MenuItem key={index} link={link} />
           ))}
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
