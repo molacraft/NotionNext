aad1458b12d99ce7dc32e1b177865eb4547ba9fe
molacraft <info@molacraft.com>
2026-03-03 10:33:14 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -372,7 +372,7 @@ const CONFIG = {
   STARTER_CTA_BUTTON_TEXT: 'Contact us→',
 
   STARTER_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
-  STARTER_POST_REDIRECT_URL: ', // 重定向域名
+  STARTER_POST_REDIRECT_URL: '', // 重定向域名
   STARTER_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_STARTER_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
 export default CONFIG
